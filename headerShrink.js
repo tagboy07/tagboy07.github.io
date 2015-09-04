6ef71bf452643380edff91b7348653fdefd540e3
tagboy07 <tagboy07@gmail.com>
2015-09-04 12:33:55 -0500

    Initial Commit

---
+++ headerShrink.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('header').data('height','big');
+});
+
+$(window).scroll(function(){
+    var $nav = $('header');
+    if ($('body').scrollTop() > 68) {
+        if ($nav.data('height') == 'big') {
+            $nav.data('height','small').stop().css("height", "72px");
+            $('header').css("position", "fixed");
+        }
+    } else {
+        if ($nav.data('height') == 'small') {
+            $nav.data('height','big').stop().css("height", "200px");
+            $('header').css("position", "relative");
+        }  
+    }
+});
