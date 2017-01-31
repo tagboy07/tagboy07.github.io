2c98c9c1a3fc35b376b33d4844bd3adfa7d29875
Troy Goodin <tagboy07@gmail.com>
2017-01-31 04:13:59 -0600

    Done with 3d icons

--- headerShrink.js
+++ headerShrink.js
@@ -1,20 +1,20 @@
-$(function(){
-    $('header').data('height','big');
-});
-
-$(window).scroll(function(){
-    var $nav = $('header');
-    if ($('body').scrollTop() > 68) {
-        if ($nav.data('height') == 'big') {
-            $nav.data('height','small').stop().css("height", "100px");
-            $('header').css("position", "fixed");
-        }
-    } else {
-		
-        if ($nav.data('height') == 'small') {
-            $nav.data('height','big').stop().css("height", "200px");
-            $('header').css("position", "relative");
-			window.scrollTo(0,0);
-        }  
-    }
-});
+//$(function(){
+//    $('header').data('height','big');
+//});
+//
+//$(window).scroll(function(){
+//    var $nav = $('header');
+//    if ($('body').scrollTop() > 68) {
+//        if ($nav.data('height') == 'big') {
+//            $nav.data('height','small').stop().css("height", "100px");
+//            $('header').css("position", "fixed");
+//        }
+//    } else {
+//		
+//        if ($nav.data('height') == 'small') {
+//            $nav.data('height','big').stop().css("height", "125px");
+//            $('header').css("position", "relative");
+//			window.scrollTo(0,0);
+//        }  
+//    }
+//});
